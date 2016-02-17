216b1cdcdf4a1d80a4d496bfb49470ea03d2f10d
vovcheek <volodymyr.vasylyk@gmail.com>
2016-02-17 07:15:44 -0600

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -5,8 +5,8 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   fileP = file.path(paste(getwd(), "/", directory, "/", monitors[id[1]], sep = ''))
   workSet <- read.csv(fileP, header = TRUE, sep=',')
   
-  if(length(id)>1){
-    for(i in id[2:length(id)]){
+  if(length(id) > 1){
+    for(i in id[2:length(id)]) {
       fileP = file.path(paste(getwd(), "/", directory, "/", monitors[i], sep = ''))
       workSet <- rbind(workSet, read.csv(fileP, header = TRUE, sep=','))
     }
