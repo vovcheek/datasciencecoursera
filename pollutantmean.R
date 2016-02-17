2c7635a127c9e8dba59e37e7db07cf852d942f62
vovcheek <volodymyr.vasylyk@gmail.com>
2016-02-17 07:14:45 -0600

    Create pollutantmean.R

---
+++ pollutantmean.R
@@ -0,0 +1,19 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  location <- paste(getwd(), "/", directory, sep = '')
+  
+  monitors <- list.files(location)
+  fileP = file.path(paste(getwd(), "/", directory, "/", monitors[id[1]], sep = ''))
+  workSet <- read.csv(fileP, header = TRUE, sep=',')
+  
+  if(length(id)>1){
+    for(i in id[2:length(id)]){
+      fileP = file.path(paste(getwd(), "/", directory, "/", monitors[i], sep = ''))
+      workSet <- rbind(workSet, read.csv(fileP, header = TRUE, sep=','))
+    }
+  }
+  
+  workSetNoNA <- workSet[complete.cases(workSet),]
+  meanPollutant <- mean(workSetNoNA[[pollutant]])
+  
+  return(meanPollutant)
+}
